f0a7eacf18aa2925d055bc054035d4231018a125
DESKTOP-3B3T42I\felip <felipe_.araujo@hotmail.com>
2020-11-26 17:58:59 -0300

    Banco de Dados FAKE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    HomeComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule
