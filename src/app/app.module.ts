da425e0dc40c33cf1a37f2b6b09051429382aed5
Felipe_Ara <felipe_.araujo@hotmail.com>
2020-12-10 16:20:59 -0300

    Link do topo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
+
+import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
 import { TopoComponent } from './topo/topo.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
     HomeComponent,
-    TopoComponent
+    TopoComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
